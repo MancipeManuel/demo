f41186151a36475f1ec3477b7f20a1ac447708cf
MancipeManuel <162451732+MancipeManuel@users.noreply.github.com>
2025-02-10 16:04:07 -0500

    arreglo algo

--- demo/src/main/java/com/example/demo/domain/model/DocumentType.java
+++ demo/src/main/java/com/example/demo/domain/model/DocumentType.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class DocumentTipe {
+public class DocumentType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
